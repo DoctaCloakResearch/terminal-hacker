86edff71497e18be197f18ddfa10e2d18e2c309e
Christopher Taylor <doctacloak@protonmail.com>
2018-10-21 14:26:52 -0500

    added main menu

---
+++ Assets/Scripts/Hacker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		ShowMainMenu();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void ShowMainMenu () {
+		Terminal.ClearScreen();
+		Terminal.WriteLine ("What would you like to hack into?");
+		Terminal.WriteLine ("Press 1 for the local library");
+		Terminal.WriteLine ("Press 2 for the local police station");
+		Terminal.WriteLine ("Press 3 for NASA");
+
+		Terminal.WriteLine ("Enter your selection: ");
+	}
+}
